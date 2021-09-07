19a26a447d56967c259a23472d8ba5ced735110b
Weisse_Schoko <84969121+WeisseSchokolade@users.noreply.github.com>
2021-09-07 11:25:13 +0900

    Update simplecalc.js

--- simplecalc.js
+++ simplecalc.js
@@ -1,7 +1,5 @@
 var arg1 = document.getElementsByName("arg1");
 var answer_input = document.getElementsByName("answer");
-
 var answer = eval(arg1)
-
 inputF.value = answer;
 answer_input.innerHTML = "Value = " + "'" + inputF.value + "'";
