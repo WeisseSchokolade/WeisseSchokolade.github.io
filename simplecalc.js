2c550fdc631a5f94abf1536737f2db44fcf5f1bb
Weisse_Schoko <84969121+WeisseSchokolade@users.noreply.github.com>
2021-09-07 11:27:59 +0900

    Update simplecalc.js

--- simplecalc.js
+++ simplecalc.js
@@ -1,5 +1,4 @@
 var arg = document.getElementsByName("arg1");
 var answer_input = document.getElementsByName("answer");
 var answer = eval(arg)
-inputF.value = answer;
-answer_input.innerHTML = "Value = " + "'" + inputF.value + "'";
+answer_input.value = answer;
